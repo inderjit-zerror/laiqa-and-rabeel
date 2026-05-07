b167dbf805a63b35149c0ed3cb1f93329fa8d0f2
inderjit-zerror <inderjit.zerror@gmail.com>
2026-05-07 17:57:38 +0530

    done

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -251,7 +251,7 @@ const NavBar = () => {
       </div>
 
       {/* MOBILENAV */}
-      <div className="w-full flex flex-col lg:hidden justify-center text-[1.5rem] leading-[1.5rem] z-100 uppercase gap-4 items-center MOBILENAV h-screen COLOR_BG_CREAM absolute -top-10 left-[150%] z-[-1]">
+      <div className="w-full flex flex-col lg:hidden justify-center text-[1.5rem] leading-[1.5rem] z-100 uppercase gap-4 items-center MOBILENAV h-screen bg-white absolute -top-10 left-[150%] z-[-1]">
         <Link href={`/`}>
           <div
             onClick={clickCheck}
